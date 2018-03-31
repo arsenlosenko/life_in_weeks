7a6333743ae9153fe08663a44abb04acadcfedc9
Arsen Losenko <arsenlosenko@gmail.com>
2018-03-31 22:28:07 +0300

    added routing, rendered templates

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return 'hello world!'
+def index():
+    # ask for initial info (age), tell story about life calendar
+    return render_template('index.html')
 
+@app.route("/calendar")
+def calendar():
+    # show calendar with highlighted weeks according to age (pass age from index)
+    return render_template('calendar.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
