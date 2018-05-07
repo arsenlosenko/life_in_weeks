e2da28b6bcf7036539660eee00a35e52198d1f55
Arsen Losenko <arsenlosenko@gmail.com>
2018-05-07 14:30:49 +0300

    Change class name and usage of context in drawWeekGrid()

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-class CanvasConfig {
+class LifeCalendar {
     constructor () {
-        this.canvas = document.getElementById('tutorial');
+        this.canvas = document.getElementById('lifeCalendar');
         this.colorPicker = document.getElementById('colorPicker');
         this.ctx = this.canvas.getContext('2d');
         this.lineFill = '#000000';
-        this.gridPoints = this.drawWeekGrid(this.ctx);
+        this.gridPoints = [];
         this.canDraw = false;
+        this.drawWeekGrid();
         this.setDrawEvent()
         this.setColorPickerChange();
         this.drawOnGrid(); 
@@ -45,36 +46,31 @@ class CanvasConfig {
     draw(e){ 
         if (this.canDraw){ 
             let pos = this.getMousePos(e);
-            let posx = pos.x;
-            let posy = pos.y;
-            let point = [posx,posy];
-            let matchPoints = this.gridPoints.find(function (x) { 
+            let point = [pos.x,pos.y];
+            let matchPoints = this.gridPoints.find((x) => { 
                                                 return (
                                                         (point[0] > (x[0] - 10) && point[0] < (x[0] + 10)) 
                                                         && (point[1] > (x[1] - 10) && point[1] > (x[1]  + 10)) 
                                                     )});
             if(matchPoints){
                 this.ctx.fillStyle = this.lineFill;
-                this.ctx.fillRect(posx, posy,  20, 20);
+                this.ctx.fillRect(pos.x, pos.y,  20, 20);
             }
         }
     }
 
-    drawWeekGrid(ctx){
-        let gridPoints = [];
+    drawWeekGrid(){
         for(let x = 10; x <= 520; x+= 10){ 
-           ctx.fillText(x/10, x * 2, 10);
+           this.ctx.fillText(x/10, x * 2, 10);
            for(let y = 10; y <= 900; y += 10){
                   let gridPoint = [x * 2, y * 2];
-                  ctx.fillText(y/10, 5, y*2);
-                  ctx.strokeRect(x * 2, y * 2, 10, 10); 
-                  gridPoints.push(gridPoint);
+                  this.ctx.fillText(y/10, 5, y*2);
+                  this.ctx.strokeRect(x * 2, y * 2, 10, 10); 
+                  this.gridPoints.push(gridPoint);
               }
          }
-        return gridPoints;
     }
-    
 
 }
 
-document.addEventListener('DOMContentLoaded', new CanvasConfig());
+document.addEventListener('DOMContentLoaded', new LifeCalendar());
