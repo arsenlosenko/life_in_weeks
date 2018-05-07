021c8684c9db6b57055e17975eed1799c3172dd9
Arsen Losenko <arsenlosenko@gmail.com>
2018-05-07 14:20:32 +0300

    Refactor to match ES6

--- index.js
+++ index.js
@@ -27,38 +27,45 @@ class CanvasConfig {
         });
     }
 
-
-
     drawOnGrid() {
         this.canvas.addEventListener('mousemove', (e) => {
-            if (this.canDraw){ 
-                var rect = this.canvas.getBoundingClientRect();
-                var pos = { 
-                    x: e.clientX - rect.left,
-                    y: e.clientY - rect.top 
-                }
-                var posx = pos.x;
-                var posy = pos.y;
-                var point = [posx,posy];
-                var matchPoints = this.gridPoints.find(function (x) { 
-                                                    return (
-                                                            (point[0] > x[0] && point[0] < (x[0] + 10)) 
-                                                            && (point[1] > x[1] && point[1] > (x[1]  + 10)) 
-                                                        )});
-                if(matchPoints){
-                    this.ctx.fillStyle = this.lineFill;
-                    this.ctx.fillRect(posx, posy,  20, 20);
-                }
-            }
+            this.draw(e);
         });
     }
 
+    getMousePos(e){
+        let rect = this.canvas.getBoundingClientRect();
+        return { 
+            x: e.clientX - rect.left,
+            y: e.clientY - rect.top 
+        }
+    }
+
+
+    draw(e){ 
+        if (this.canDraw){ 
+            let pos = this.getMousePos(e);
+            let posx = pos.x;
+            let posy = pos.y;
+            let point = [posx,posy];
+            let matchPoints = this.gridPoints.find(function (x) { 
+                                                return (
+                                                        (point[0] > (x[0] - 10) && point[0] < (x[0] + 10)) 
+                                                        && (point[1] > (x[1] - 10) && point[1] > (x[1]  + 10)) 
+                                                    )});
+            if(matchPoints){
+                this.ctx.fillStyle = this.lineFill;
+                this.ctx.fillRect(posx, posy,  20, 20);
+            }
+        }
+    }
+
     drawWeekGrid(ctx){
-        var gridPoints = [];
-        for(var x = 10; x <= 520; x+= 10){ 
+        let gridPoints = [];
+        for(let x = 10; x <= 520; x+= 10){ 
            ctx.fillText(x/10, x * 2, 10);
-           for(var y = 10; y <= 900; y += 10){
-                  var gridPoint = [x * 2, y * 2];
+           for(let y = 10; y <= 900; y += 10){
+                  let gridPoint = [x * 2, y * 2];
                   ctx.fillText(y/10, 5, y*2);
                   ctx.strokeRect(x * 2, y * 2, 10, 10); 
                   gridPoints.push(gridPoint);
